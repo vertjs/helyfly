9aac2904a7b8f1a9303b610a9dadb10c70ef4b61
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2020-11-05 00:59:53 +0300

    fix content

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,10 +1,10 @@
 let ready = () => {
-	document.querySelectorAll(".ibg").forEach(el => {
-		if(el.querySelector('img')) {
-			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
-			el.querySelector('img').style.display = 'none';
-		}
-	});
+  document.querySelectorAll(".ibg").forEach(el => {
+    if(el.querySelector('img')) {
+      el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
+      el.querySelector('img').style.visibility = 'hidden';
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", ready);
