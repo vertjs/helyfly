36e7367564de77fdb0dceb4ac54d22b55d8ea77c
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2020-11-15 21:50:47 +0300

    js - viewport

--- helyfly/js/script.js
+++ helyfly/js/script.js
@@ -4,17 +4,23 @@ let ready = () => {
       el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
       el.querySelector('img').style.minWidth = '100%';
       el.querySelector('img').style.visibility = 'hidden';
-     
     }
   });
 
+  if(screen.width < 920) {
+    console.log(document.querySelector('.section-1__item-2'))
+
+    document.querySelector('.section-1__item-1').insertAdjacentHTML('afterend', 
+    '<div class="section-1__item-2"><img src="./img/photo/posterimage.jpg" alt="kiss"><iframe src="https://www.youtube.com/embed/W4YJ0jMmp_o?rel=0" frameborder="0" allow="accelerometer" allowfullscreen encrypted-media gyroscope picture-in-picture data-play="0"></iframe><button></button>Your browser does not support the video or formats.</div>'
+    )
+  }
 }
 
 document.addEventListener("DOMContentLoaded", ready);
 
-const buttonPlay = document.querySelector('.section-1__item-2__bg.ibg').querySelector('button')
+const buttonPlay = document.querySelector('.section-1__item-2').querySelector('button')
 
-const ifr = document.querySelector('.section-1__item-2__bg.ibg').querySelector('iframe')
+const ifr = document.querySelector('.section-1__item-2').querySelector('iframe')
 const listMarker =document.querySelector('.section-1__item-1').querySelectorAll('li')
 
 /* added background-image to li, instead standart marker  */
