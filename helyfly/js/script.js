660a83180d09d18fad55bd00fc14f2fb1a8fd1e1
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2020-11-19 23:23:45 +0300

    give it a clean to depiction

--- helyfly/js/script.js
+++ helyfly/js/script.js
@@ -1,3 +1,5 @@
+const listMarker = document.querySelector('.section-1__item-1').querySelectorAll('li')
+
 let ready = () => {
   document.querySelectorAll(".ibg").forEach(el => {
     if(el.querySelector('img')) {
@@ -6,13 +8,21 @@ let ready = () => {
       el.querySelector('img').style.visibility = 'hidden';
     }
   });
-
+  Array.from(listMarker).forEach((el, i) => marksScreenDesktop(el, i))
   if(screen.width < 920) {
-    console.log(document.querySelector('.section-1__item-2'))
-
-    document.querySelector('.section-1__item-1').insertAdjacentHTML('afterend', 
-    '<div class="section-1__item-2"><img src="./img/photo/posterimage.jpg" alt="kiss"><iframe src="https://www.youtube.com/embed/W4YJ0jMmp_o?rel=0" frameborder="0" allow="accelerometer" allowfullscreen encrypted-media gyroscope picture-in-picture data-play="0"></iframe><button></button>Your browser does not support the video or formats.</div>'
+    document.querySelector('.section-1__item-1').insertBefore(
+      document.querySelector('.section-1__item-2'),
+      document.querySelector('.section-1__item-1 > div')
     )
+    Array.from(listMarker).forEach((el, i) => marksScreenMobile(el, i))
+  
+  }
+  else {
+    document.querySelector('.section-1__item-1').insertBefore(
+      document.querySelector('.section-1__item-1 > div'),
+      document.querySelector('.section-1__item-2')
+    )
+    Array.from(listMarker).forEach((el, i) => marksScreenDesktop(el, i))
   }
 }
 
@@ -21,13 +31,14 @@ document.addEventListener("DOMContentLoaded", ready);
 const buttonPlay = document.querySelector('.section-1__item-2').querySelector('button')
 
 const ifr = document.querySelector('.section-1__item-2').querySelector('iframe')
-const listMarker =document.querySelector('.section-1__item-1').querySelectorAll('li')
 
-/* added background-image to li, instead standart marker  */
-Array.from(listMarker).forEach((el, i) => { 
-  el.style.background = 'url(' + '../img/icons/list/0' + ++i +'.svg' + ') no-repeat left center';
-})
+function marksScreenMobile(el, i) {
+  el.style.background = 'url(' + '../img/icons/list_m/0' + ++i +'.svg' + ') no-repeat left center';
+}
 
+function marksScreenDesktop(el, i) {
+  el.style.background = 'url(' + '../img/icons/list_d/0' + ++i +'.svg' + ') no-repeat left center';
+}
 /* display video on click */
 buttonPlay.addEventListener('click', videoPlay) 
 
@@ -36,3 +47,4 @@ function videoPlay(event) {
   event.target.remove()
   event.preventDefault();
 } 
+
